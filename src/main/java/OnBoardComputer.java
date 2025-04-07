c98a9c644c9d59d6196eef7ae5ecf497aceae306
Nathan <natefintak@gmail.com>
2025-04-07 09:04:32 -0400

    All tests passing

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -4,16 +4,12 @@ public class OnBoardComputer implements BurnStream {
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
 
-        if (status.getAltitude() <= 5500 && status.getAltitude() > 550) {
+        if (status.getAltitude() <= 5900 && status.getAltitude() > 550 && status.getVelocity() > 100) {
             burn = 200;
         } else if (status.getAltitude() < 550 && status.getVelocity() > 150) {
             burn = 200;
-        } else if (status.getAltitude() <= 150 && status.getAltitude() >= 50) {
-            burn = (status.getVelocity() / 2) + 100;
-        } else if (status.getAltitude() <= 50 && status.getAltitude() >= 25) {
-            burn = 120;
-        } else if (status.getAltitude() < 10 && status.getVelocity() >= 5) {
-            burn = 103;
+        } else if (status.getAltitude() <= 100 && status.getVelocity() > 3) {
+            burn = 198;
         } else {
             burn = 100;
         }
@@ -21,11 +17,5 @@ public class OnBoardComputer implements BurnStream {
 
         System.out.println(burn); /*hack!*/
         return burn;
-
-        //similar to jumper?
-        //altitude / velocity + altitude % velocity = burn?
-        //altitude / velocity?
-        //altitude % velocity?
-        //check the dataStream in tests, try to figure out the pattern with that
     }
 }
